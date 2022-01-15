2a433c23086a5ec1ff3dc4271da83599b25938c6
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-01-14 21:33:38 -0800

    Add new project

---
+++ src/display.rs
@@ -0,0 +1,29 @@
+use termion::{color, style};
+
+#[macro_export]
+macro_rules! bold_color_text {
+    ($a:expr, $b:expr) => {{
+        format!(
+            "{}{}{}{}{}",
+            termion::style::Bold,
+            termion::color::Fg($b),
+            $a,
+            termion::color::Fg(termion::color::Reset),
+            termion::style::Reset,
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! warn_user {
+    ($a:expr) => {{
+        println!(
+            "{}{}WARN:{}{} {}",
+            termion::style::Bold,
+            termion::color::Fg(termion::color::Yellow),
+            termion::color::Fg(termion::color::Reset),
+            termion::style::Reset,
+            $a,
+        );
+    }};
+}
