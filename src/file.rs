de87a05fe2195db8b1c0da5ba9b3dcc4ae13fb86
Jake <jakeroggenbuck2@gmail.com>
2022-11-05 00:53:56 -0700

    Add more dynamic files

--- src/file.rs
+++ src/file.rs
@@ -1,49 +1,15 @@
-pub enum FileTypes {
-    MitLicense,
-    Gplv3License,
-    Python,
-    PythonArg,
-    LatexMathHomework,
-    Latex,
-    NoFile,
-    MainC,
-    MakeWall,
-    HTML,
-}
+use std::collections::HashMap;
 
-pub trait Filename {
-    fn outname(&self) -> &str;
-    fn uniquename(&self) -> &str;
-}
-
-impl Filename for FileTypes {
-    fn outname(&self) -> &str {
-        match self {
-            FileTypes::MitLicense => "LICENSE",
-            FileTypes::Gplv3License => "LICENSE",
-            FileTypes::Python => "main.py",
-            FileTypes::PythonArg => "main.py",
-            FileTypes::LatexMathHomework => "main.tex",
-            FileTypes::Latex => "main.tex",
-            FileTypes::MainC => "main.c",
-            FileTypes::MakeWall => "Makefile",
-            FileTypes::NoFile => "NoFile",
-            FileTypes::HTML => "index.html",
-        }
-    }
-
-    fn uniquename(&self) -> &str {
-        match self {
-            FileTypes::MitLicense => "MIT_LICENSE",
-            FileTypes::Gplv3License => "GPLV3_LICENSE",
-            FileTypes::Python => "PYTHON",
-            FileTypes::PythonArg => "PYTHON_ARG",
-            FileTypes::LatexMathHomework => "LATEX_MATH_HOMEWORK",
-            FileTypes::Latex => "LATEX",
-            FileTypes::MainC => "MAIN_C",
-            FileTypes::MakeWall => "MAKE_WALL",
-            FileTypes::NoFile => "NO_FILE",
-            FileTypes::HTML => "HTML",
-        }
-    }
+pub fn get_files() -> HashMap<&'static str, &'static str> {
+    HashMap::from([
+        ("mit", "LICENSE"),
+        ("gpl", "LICENSE"),
+        ("py", "main.py"),
+        ("pyarg", "main.py"),
+        ("mathlatex", "main.tex"),
+        ("latex", "main.tex"),
+        ("mainc", "main.c"),
+        ("makewall", "Makefile"),
+        ("html", "index.html"),
+    ])
 }
